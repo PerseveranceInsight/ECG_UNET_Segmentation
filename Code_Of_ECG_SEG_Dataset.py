41d32da21754606531ba228b3a534944a6a152fc
neil.yu <neil.yu@egsitec.com>
2021-07-27 09:48:39 +0800

    Modify Code_Of_ECG_SEG_Dataset

--- Code_Of_ECG_SEG_Dataset.py
+++ Code_Of_ECG_SEG_Dataset.py
@@ -2,48 +2,65 @@ import numpy as np
 import glob as gb
 import sys
 import os
+import random
+import datetime
 
-class ECG_SEG_Dataset():
-    def __init__(self, dataset_path):
-        import wfdb
-        from wfdb import processing
-        leads_suffix = {0: 'i',
-                        1: 'ii',
-                        2: 'iii',
-                        3: 'avr',
-                        4: 'avl',
-                        5: 'avf',
-                        6: 'v1',
-                        7: 'v2',
-                        8: 'v3',
-                        9: 'v4',
-                        10: 'v5',
-                        11: 'v6'}
-        header_suffix = '.hea'
-        signal_suffix = '.dat'
+import torch
+from torch.utils.data import Dataset, DataLoader
 
-        part_ind = 1
-        part_path = dataset_path + str(part_ind)
-        print('part_path : {0}'.format(part_path))
+class ECG_SEG_Dataset(Dataset):
+    def __init__(self, 
+                 preprocess_dataset_path,
+                 mode='train'):
+        signals_con_path = preprocess_dataset_path + 'signals_con.npy'
+        lead_cons_path = preprocess_dataset_path + 'lead_anns_con.npy'
+        signals = np.load(signals_con_path)
+        lead_cons = np.load(lead_cons_path)
+        # print('Shape of signals : {0}'.format(signals.shape))
+        # print('Shape of lead_cons : {0}'.format(lead_cons.shape))
+        num_sample = signals.shape[0]
+        if mode == 'train':
+            mode_id = np.ones((num_sample), dtype=bool)
+            mode_id[9::10] = 0
+        else:
+            mode_id = np.zeros((num_sample), dtype=bool)
+            mode_id[9::10] = 1
+        signals = signals[mode_id, :]
+        lead_cons = lead_cons[mode_id, :]
+        num_sample = signals.shape[0]
+        shuffle_id = np.arange(num_sample)
+        np.random.shuffle(shuffle_id)
+        # print('mode_id : {0}'.format(mode_id))
+        # print('Shape of signals : {0}'.format(signals.shape))
+        # print('Shape of lead_cons : {0}'.format(lead_cons.shape))
+        # print('shuffle_id : {0}'.format(shuffle_id))
+        signals = signals[shuffle_id, :]
+        lead_cons = lead_cons[shuffle_id, :]
+        self.signals = torch.FloatTensor(signals)
+        self.lead_cons = torch.LongTensor(lead_cons)
 
-        i_lead = wfdb.rdann(part_path, leads_suffix[0]) 
-        ii_lead = wfdb.rdann(part_path, leads_suffix[1])
-        iii_lead = wfdb.rdann(part_path, leads_suffix[2])
-        avr_lead = wfdb.rdann(part_path, leads_suffix[3])
-        avl_lead = wfdb.rdann(part_path, leads_suffix[4])
-        avf_lead = wfdb.rdann(part_path, leads_suffix[5])
-        v1_lead = wfdb.rdann(part_path, leads_suffix[6])
-        v2_lead = wfdb.rdann(part_path, leads_suffix[7])
-        v3_lead = wfdb.rdann(part_path, leads_suffix[8])
-        v4_lead = wfdb.rdann(part_path, leads_suffix[9])
-        v5_lead = wfdb.rdann(part_path, leads_suffix[10])
-        v6_lead = wfdb.rdann(part_path, leads_suffix[11])
+    def __getitem__(self, index):
+        random.seed(datetime.time())
+        sample_freq = 500 # 500 Hz
+        sample_span = 4 # 4 sec
+        total_sample_span = 6
+        span_len = sample_freq * sample_span
+        signals = self.signals[index, :]
+        lead_cons = self.lead_cons[index, :]
+        stride_start_valid_ind = total_sample_span * sample_freq - span_len
+        stride_start_ind = random.randint(a = 0,
+                                          b = stride_start_valid_ind)
+        # print('Shape of signals : {0}'.format(signals.shape))
+        # print('Shape of lead_cons : {0}'.format(lead_cons.shape))
+        # print('stride_start_ind : {0}'.format(stride_start_ind))
+        signals = signals[stride_start_ind:stride_start_ind + span_len]
+        lead_cons = lead_cons[stride_start_ind:stride_start_ind + span_len]
+        # print('Shape of signals : {0}'.format(signals.shape))
+        # print('Shape of lead_cons : {0}'.format(lead_cons.shape))
+        return signals, lead_cons
 
-        print('i_lead : {0}'.format(i_lead.sample))
-        
-        signal_sample = wfdb.rdsamp(part_path)
-        print('signal_sample : {0}'.format(signal_sample))
-
+    def __len__(self):
+        return len(self.signals)
 
 
 if __name__ == '__main__':
@@ -78,8 +95,10 @@ if __name__ == '__main__':
         signal = signal_sample[0]
         signal_len = signal.shape[0]
         lead_anns = []
+        lead_valid = []
      
         for lead_idx in range(12):
+            sample_quality = True
             lead_ann_sym = wfdb.rdann(part_path, leads_suffix[lead_idx], return_label_elements=['symbol']).symbol
             lead_ann_time = np.asarray(wfdb.rdann(part_path, leads_suffix[lead_idx]).sample)
             lead_ann_len = len(lead_ann_sym)
@@ -103,16 +122,25 @@ if __name__ == '__main__':
                     break
             if sample_quality:
                 lead_anns.append(lead_ann)
+                lead_valid.append(lead_idx)
             else:
-                break
+                continue
+
+        # print('lead_valid : {0}'.format(lead_valid))
+        sample_quality = (len(lead_valid) != 0)
+        # print('sample_quality : {0}'.format(sample_quality))
         
         if sample_quality:
-            print('Save sample : {0}'.format(part_ind))
-            signal_npy_path = numpydataset_path + 'lu_dataset_raw_npy/' + '{0:03d}'.format(part_ind) + '_sig.npy'
-            lead_anns_npy_path = numpydataset_path + 'lu_dataset_raw_npy/' + '{0:03d}'.format(part_ind) + '_lead_anns.npy'
-            print('signal_npy_path : {0}'.format(signal_npy_path))
-            print('lead_anns_npy_path : {0}'.format(lead_anns_npy_path))
+            # print('Save sample : {0}'.format(part_ind))
+            signal_npy_path = numpydataset_path + '{0:03d}'.format(part_ind) + '_sig.npy'
+            lead_anns_npy_path = numpydataset_path + '{0:03d}'.format(part_ind) + '_lead_anns.npy'
+            # print('signal_npy_path : {0}'.format(signal_npy_path))
+            # print('lead_anns_npy_path : {0}'.format(lead_anns_npy_path))
             lead_anns = np.asarray(lead_anns)
+            # print('lead_anns : {0}'.format(lead_anns.shape))
+            # print('signal : {0}'.format(signal.shape))
+            signal = signal[:, lead_valid]
+            # print('signal : {0}'.format(signal.shape))
             np.save(signal_npy_path, signal)
             np.save(lead_anns_npy_path, lead_anns)
         else:
@@ -182,22 +210,20 @@ if __name__ == '__main__':
         signals_con = []
 
         for part_ind in range(1, 201):
+            # print('part_ind : {0}'.format(part_ind))
             lead_anns, signals = one_signal_preprocessing(numpydataset_path,
                                                           part_ind)
             if lead_anns is None:
                 print('lead_anns and signals is None {0}'.format(part_ind))
             else:
-                lead_anns_con.append(lead_anns)
-                signals_con.append(signals)
+                if part_ind == 1:
+                    lead_anns_con = lead_anns
+                    signals_con = signals
+                else:
+                    lead_anns_con = np.concatenate((lead_anns_con, lead_anns), axis=0)
+                    signals_con = np.concatenate((signals_con, signals), axis=0)
 
-        lead_anns_con = np.asarray(lead_anns_con)
-        signals_con = np.asarray(signals_con)
-        # print('Shape of lead_anns_con : {0} {1}'.format(lead_anns_con.shape[0] * lead_anns_con.shape[1], lead_anns_con.shape[2]))
-        # print('Shape of signals_con : {0}'.format(signals_con.shape))
-        lead_anns_con = lead_anns_con.reshape(-1, lead_anns_con.shape[2])
-        signals_con = signals_con.reshape(-1, signals_con.shape[2])
-
-        # print('Shape of lead_anns_con : {0}'.format(lead_anns_con.shape))
+        # print('Shape of lead_anns_con : {0} '.format(lead_anns_con.shape))
         # print('Shape of signals_con : {0}'.format(signals_con.shape))
         # print('numpydataset_preprocess_path : {0}'.format(numpydataset_preprocess_path))
         lead_anns_con_path = numpydataset_preprocess_path + 'lead_anns_con.npy'
@@ -225,5 +251,7 @@ if __name__ == '__main__':
             elif sys.argv[1] == 'signal_preprocessing':         
                 signal_preprocessing(numpydataset_path = sys.argv[2],
                                      numpydataset_preprocess_path = sys.argv[3])
-        else:
-            ecg_dataset = ECG_SEG_Dataset(dataset_path = './lu_dataset/data/')
+        elif len(sys.argv) == 2:
+            ecg_dataset = ECG_SEG_Dataset(preprocess_dataset_path=sys.argv[1])
+            sig, lead_ann = ecg_dataset.__getitem__(index = 1)
+            print(ecg_dataset.__len__())
